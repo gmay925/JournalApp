bf2bcb924eb6572133270edf1df7db80e009b20b
Gia May <gia.may@wwt.com>
2022-05-26 09:58:43 -0400

    second push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 
 function App() {
+  const [data, setData] = useState(0);
+
+  useEffect(() => {
+    fetch('/api')
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{!data ? 'Loading..' : data}</p>
+      <p> You clicked {data} times</p>
+      <button onClick={() => setData(data + 1)}>Click me</button>
     </div>
   );
 }
