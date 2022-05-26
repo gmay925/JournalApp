99438c1a37b7b8847a2a25326a4c08083094c8a7
Gia May <gia.may@wwt.com>
2022-05-26 10:20:23 -0400

    third push

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+app.use(logger('dev'));
+app.use(cors());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
 
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello from server!' });
